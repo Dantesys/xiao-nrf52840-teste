95f2e052388f5b976f1d88ee124b363e50788379
Dantesys <60984773+Dantesys@users.noreply.github.com>
2025-05-26 15:08:27 -0300

    FEAT: Adicionando comentarios no xiao-nrf52840-master.ino

--- xiao-nrf52840-master.ino
+++ xiao-nrf52840-master.ino
@@ -1,5 +1,7 @@
+//Acelerometro e Giroscopio
 #include <LSM6DS3.h>
 #include <Wire.h>
+//Bluetooth
 #include <ArduinoBLE.h>
 //Tensorflow
 #include <TensorFlowLite.h>
@@ -9,20 +11,20 @@
 #include <tensorflow/lite/schema/schema_generated.h>
 //Modelo de Movimentos
 #include "model.h"
-
-const float accelerationThreshold = 2.5; // threshold of significant in G's
+// limiar de significativo em G's
+const float limiteAcelerecao = 2.5;
 const int numSamples = 119;
 
 int samplesRead = numSamples;
 
 LSM6DS3 myIMU(I2C_MODE, 0x6A);  
 
-// global variables used for TensorFlow Lite (Micro)
+// Variaveis globais para o TensorFlow Lite Micro (TFLM)
 tflite::MicroErrorReporter tflErrorReporter;
 
-// pull in all the TFLM ops, you can remove this line and
-// only pull in the TFLM ops you need, if would like to reduce
-// the compiled size of the sketch.
+// Extrai todas as operações do TFLM
+// Você pode remover esta linha e extrair apenas as operações do TFLM necessárias
+// se quiser reduzir o tamanho compilado do esboço.
 tflite::AllOpsResolver tflOpsResolver;
 
 const tflite::Model* tflModel = nullptr;
@@ -30,8 +32,8 @@ tflite::MicroInterpreter* tflInterpreter = nullptr;
 TfLiteTensor* tflInputTensor = nullptr;
 TfLiteTensor* tflOutputTensor = nullptr;
 
-// Create a static memory buffer for TFLM, the size may need to
-// be adjusted based on the model you are using
+// Crie um buffer de memória estática para TFLM, o tamanho pode precisar
+// ser ajustado com base no modelo que você está usando
 constexpr int tensorArenaSize = 8 * 1024;
 byte tensorArena[tensorArenaSize] __attribute__((aligned(16)));
 
@@ -42,145 +44,155 @@ const char* GESTURES[] = {
   "direita",
   "esquerda"
 };
-
 #define NUM_GESTURES (sizeof(GESTURES) / sizeof(GESTURES[0]))
+
+//Definindo o uuid e a caracteristica do serviço de acesso do bluetooth
 const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
 const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
 
-int gesture = -1;
-int oldGestureValue = -1;
+int movimento = -1;
+int movimentoAnterior = -1;
+
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-
+  //Verificando IMU
   if (myIMU.begin() != 0) {
-    Serial.println("Device error");
+    Serial.println("Erro no dispositivo");
   } else {
-    Serial.println("Device OK!");
+    Serial.println("Dispositivo OK!");
   }
 
   Serial.println();
 
-  // get the TFL representation of the model byte array
+  // obtem a representação TFL da matriz de bytes do modelo
   tflModel = tflite::GetModel(model);
   if (tflModel->version() != TFLITE_SCHEMA_VERSION) {
-    Serial.println("Model schema mismatch!");
+    Serial.println("Incompatibilidade de esquema do modelo!");
     while (1);
   }
-
-  // Create an interpreter to run the model
+  // Cria um intérprete para executar o modelo
   tflInterpreter = new tflite::MicroInterpreter(tflModel, tflOpsResolver, tensorArena, tensorArenaSize, &tflErrorReporter);
 
-  // Allocate memory for the model's input and output tensors
+  // Alocar memória para os tensores de entrada e saída do modelo
   tflInterpreter->AllocateTensors();
 
-  // Get pointers for the model's input and output tensors
+  // Obtenha ponteiros para os tensores de entrada e saída do modelo
   tflInputTensor = tflInterpreter->input(0);
   tflOutputTensor = tflInterpreter->output(0);
+
+  //Preparando Bluetooth
   if (!BLE.begin()) {
-    Serial.println("* Starting Bluetooth® Low Energy module failed!");
+    Serial.println("* Iniciando Bluetooth® Low Energy - FALHA!");
     while (1);
   }
-  
-  BLE.setLocalName("XIAO (Central)"); 
+  //Definindo nome
+  BLE.setLocalName("XIAO (MASTER)"); 
   BLE.advertise();
 
-  Serial.println("XIAO (Central Device)");
+  Serial.println("XIAO (MASTER)");
   Serial.println(" ");
 }
 
 void loop() {
-  connectToPeripheral();
+  conectarSlave();
 }
-void connectToPeripheral(){
-  BLEDevice peripheral;
-  Serial.println("- Discovering peripheral device...");
+//Funcao para procurar o slave
+void conectarSlave(){
+  BLEDevice slave;
+  Serial.println("- Descobrindo dispositivo slave...");
+  //Scanear por um servico mesmo uuid definido
   do{
     BLE.scanForUuid(deviceServiceUuid);
-    peripheral = BLE.available();
-  } while (!peripheral);
-  if (peripheral) {
-    Serial.println("* Peripheral device found!");
-    Serial.print("* Device MAC address: ");
-    Serial.println(peripheral.address());
-    Serial.print("* Device name: ");
-    Serial.println(peripheral.localName());
-    Serial.print("* Advertised service UUID: ");
-    Serial.println(peripheral.advertisedServiceUuid());
+    slave = BLE.available();
+  } while (!slave);
+  //Quando achar faca a conexao
+  if (slave) {
+    Serial.println("* Dispositivo Slave encontrado!");
+    Serial.print("* Slave MAC: ");
+    Serial.println(slave.address());
+    Serial.print("* Slave nome: ");
+    Serial.println(slave.localName());
+    Serial.print("* UUID do Serviço: ");
+    Serial.println(slave.advertisedServiceUuid());
     Serial.println(" ");
     BLE.stopScan();
-    controlPeripheral(peripheral);
+    controlaSlave(slave);
   }
 }
-
-void controlPeripheral(BLEDevice peripheral) {
-  Serial.println("- Connecting to peripheral device...");
-
-  if (peripheral.connect()) {
-    Serial.println("* Connected to peripheral device!");
+//Funcao para estabelecer conexao e e controle do slave
+void controlaSlave(BLEDevice slave) {
+  Serial.println("- Conectando dispositivo slave...");
+  //Fazendo a conexao
+  if (slave.connect()) {
+    Serial.println("* Dispositivo Slave conectado!");
     Serial.println(" ");
   } else {
-    Serial.println("* Connection to peripheral device failed!");
+    Serial.println("* Erro de conexão com o slave!");
     Serial.println(" ");
     return;
   }
-
-  Serial.println("- Discovering peripheral device attributes...");
-  if (peripheral.discoverAttributes()) {
-    Serial.println("* Peripheral device attributes discovered!");
+  Serial.println("- Descobrindo atributos do dispositivo slave...");
+  //Procurar atributos do slave
+  if (slave.discoverAttributes()) {
+    Serial.println("* Atributos do slave descobertos!");
     Serial.println(" ");
   } else {
-    Serial.println("* Peripheral device attributes discovery failed!");
+    Serial.println("* Falha na descoberta de atributos do slave!");
     Serial.println(" ");
-    peripheral.disconnect();
+    slave.disconnect();
     return;
   }
-  BLECharacteristic gestureCharacteristic = peripheral.characteristic(deviceServiceCharacteristicUuid);
-  if (!gestureCharacteristic) {
-    Serial.println("* Peripheral device does not have gesture_type characteristic!");
-    peripheral.disconnect();
+  //Acessar caracteristica pelo uuid
+  BLECharacteristic movimentoCharacteristic = slave.characteristic(deviceServiceCharacteristicUuid);
+  if (!movimentoCharacteristic) {
+    Serial.println("* O Slave não possui característica movimento!");
+    slave.disconnect();
     return;
-  } else if (!gestureCharacteristic.canWrite()) {
-    Serial.println("* Peripheral does not have a writable gesture_type characteristic!");
-    peripheral.disconnect();
+  } else if (!movimentoCharacteristic.canWrite()) {
+    Serial.println("* O Slave não possui característica movimento gravável!");
+    slave.disconnect();
     return;
   }
-  while (peripheral.connected()) {
-    gesture = gestureDetectection();
-    if (oldGestureValue != gesture) {  
-      oldGestureValue = gesture;
-      Serial.print("* Writing value to gesture_type characteristic: ");
-      Serial.println(gesture);
-      gestureCharacteristic.writeValue((byte)gesture);
-      Serial.println("* Writing value to gesture_type characteristic done!");
+  //Enquanto conectado atualiza o movimento
+  while (slave.connected()) {
+    movimento = detectarMovimento();
+    if (movimentoAnterior != movimento) {  
+      movimentoAnterior = movimento;
+      Serial.print("* Escrevendo valor para característica movimento: ");
+      Serial.println(movimento);
+      //Atualiza a caracteristica
+      movimentoCharacteristic.writeValue((byte)movimento);
+      Serial.println("* Escrita de valor para característica movimento concluída!");
       Serial.println(" ");
     }
   
   }
-  Serial.println("- Peripheral device disconnected!");
-}  
-int gestureDetectection() {
+  Serial.println("- Dispositivo Slave desconectado!");
+}
+//Funcao que usa o tensorflow a o IMU para reconhecer o movimento
+int detectarMovimento() {
   float aX, aY, aZ, gX, gY, gZ;
-  // wait for significant motion
+  // Espera por um movimento significativo
   while (samplesRead == numSamples) {
-      // read the acceleration data
+      // Leia os dados de aceleração
       aX = myIMU.readFloatAccelX();
       aY = myIMU.readFloatAccelY();
       aZ = myIMU.readFloatAccelZ();
-      // sum up the absolutes
+      // Resumir os absolutos
       float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
-      // check if it's above the threshold
-      if (aSum >= accelerationThreshold) {
-        // reset the sample read count
+      // Verifique se está acima do limite
+      if (aSum >= limiteAcelerecao) {
+        // Redefinir a contagem de leitura da amostra
         samplesRead = 0;
         break;
       }
   }
-  // check if the all the required samples have been read since
-  // the last time the significant motion was detected
+  // Verifica se todas as amostras necessárias foram lidas desde
+  // a última vez que o movimento significativo foi detectado
   while (samplesRead < numSamples) {
-    // check if new acceleration AND gyroscope data is available
-      // read the acceleration and gyroscope data
+      // Verificar se novos dados de aceleração E giroscópio estão disponíveis
+      // ler os dados de aceleração e giroscópio
       aX = myIMU.readFloatAccelX();
       aY = myIMU.readFloatAccelY();
       aZ = myIMU.readFloatAccelZ();
@@ -189,8 +201,7 @@ int gestureDetectection() {
       gY = myIMU.readFloatGyroY();
       gZ = myIMU.readFloatGyroZ();
 
-      // normalize the IMU data between 0 to 1 and store in the model's
-      // input tensor
+      // Normalizar os dados da IMU entre 0 e 1 e armazená-los no tensor de entrada do modelo
       tflInputTensor->data.f[samplesRead * 6 + 0] = (aX + 4.0) / 8.0;
       tflInputTensor->data.f[samplesRead * 6 + 1] = (aY + 4.0) / 8.0;
       tflInputTensor->data.f[samplesRead * 6 + 2] = (aZ + 4.0) / 8.0;
@@ -199,16 +210,17 @@ int gestureDetectection() {
       tflInputTensor->data.f[samplesRead * 6 + 5] = (gZ + 2000.0) / 4000.0;
       samplesRead++;
       if (samplesRead == numSamples) {
-        // Run inferencing
+        // Executa a inferência
         TfLiteStatus invokeStatus = tflInterpreter->Invoke();
         if (invokeStatus != kTfLiteOk) {
           Serial.println("Invoke failed!");
           while (1);
           return -1;
         }
-        // Loop through the output tensor values from the model
+        // Percorrer os valores do tensor de saída do modelo
         for (int i = 0; i < NUM_GESTURES; i++) {
-          if(tflOutputTensor->data.f[i]>0.75){
+          //Verifica se o movimento é 80% equivalente e o seleciona para o envio
+          if(tflOutputTensor->data.f[i]>0.8){
             Serial.print(GESTURES[i]);
             Serial.print(": ");
             Serial.println(tflOutputTensor->data.f[i], 6);
